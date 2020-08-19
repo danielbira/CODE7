e2b99037e60b8f393b4d3540d8440a2d0757a3ff
Daniel Alencastro <daniel@carusojrea.com.br>
2020-08-19 12:40:36 -0300

    Resoluções prova

---
+++ Exercicio_7.py
@@ -0,0 +1,20 @@
+n1 = int()
+n2 = int(45)
+n3 = int()
+
+print (n1 > n2)
+print (n1 < n2)
+print (n1 != n2)
+print (n1 == n2)
+print (n1 == n3)
+
+"""
+        int? n1 = null;
+        int? n2 = 45;
+        int? n3 = null;
+        Console.WriteLine(n1 > n2);
+        Console.WriteLine(n1 < n2);
+        Console.WriteLine(n1 != n2);
+        Console.WriteLine(n1 == n2);
+        Console.WriteLine(n1 == n3);             
+"""
---
+++ Exercicio_8.py
@@ -0,0 +1,3 @@
+lista = [5,3,2,4,7,1,0,6]
+
+print ('A ordem crescente da lista é', sorted(lista))
