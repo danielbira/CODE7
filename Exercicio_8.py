c6d4db6a596d6060ba68a7becf2d76bf0211c1dd
Daniel Alencastro <daniel@carusojrea.com.br>
2020-08-19 13:48:03 -0300

    Atualizando das atividades

--- Exercicio_8.py
+++ Exercicio_8.py
@@ -1,3 +1,13 @@
-lista = [5,3,2,4,7,1,0,6]
+'''lista = [5,3,2,4,7,1,0,6]'''
 
-print ('A ordem crescente da lista é', sorted(lista))
+lista = []
+for c in range(8):
+    n = int(input('Digite o Número: '))
+    if c == 0 or n > lista[-1]:
+        lista.append(n)
+    else:
+        for i in range(5):
+            if n <= lista[i]:
+                lista.insert(i,n)
+                break
+print ('A ordem crescente da lista é', (lista))
